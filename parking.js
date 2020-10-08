da5c0407c6b450f324fb09a360a857efa685ff89
Alvin Laurent Ertanto <alvin.ertanto@gmail.com>
2020-10-09 00:49:12 +0700

    add parkIn, parkOut, setParkingTime, printTicket, and pay methods

--- parking.js
+++ parking.js
@@ -1,13 +1,8 @@
 class ParkingArea {
-  constructor(
-    name,
-    location,
-    sizeOfParkingArea,
-    cash = 0
-  ) {
+  constructor(name, location, sizeOfParkingArea, cash = 0) {
     this.name = name;
     this.location = location;
-    this.sizeOfParkingArea = sizeOfParkingArea;
+    this.sizeOfParkingArea = parseFloat(sizeOfParkingArea);
     this.cash = cash;
   }
 
@@ -17,44 +12,88 @@ class ParkingArea {
 }
 
 class Vehicle {
-  constructor(name, type, size, ticketNumber, ticketPrice) {
+  constructor(
+    name,
+    type,
+    size,
+    ticketNumber,
+    ticketPrice,
+    parkStatus = false,
+    parkingTime = 0
+  ) {
     this.name = name;
     this.type = type;
-    this.size = size;
+    this.size = parseFloat(size);
     this.ticketNumber = ticketNumber;
-    this.ticketPrice = ticketPrice;
+    this.ticketPrice = parseFloat(ticketPrice);
+    this.parkStatus = parkStatus;
+    this.parkingTime = parseFloat(parkingTime);
   }
 
-  parkIn(parkingArea, ticketMachine){
-    ticketMachine.printTicket(parkingArea, this.name);
+  parkIn(parkingArea, ticketMachine) {
+    let query = ticketMachine.printTicket(parkingArea, this);
+    this.parkStatus = true;
+    console.log(
+      `${ticketMachine.name}: Welcome to ${parkingArea.name}. Please keep your ticket. Thank you.`
+    );
+    return query;
+  }
+
+  setParkingTime(time) {
+    this.parkingTime += time;
+    return `${this.name} has parked for ${time} hours`;
+  }
+
+  parkOut(parkingArea, ticketMachine) {
+    let query = ticketMachine.pay(parkingArea, this);
+    this.parkStatus = false;
+    console.log(`You parked out from ${parkingArea.name}.`);
+    return query;
   }
 }
 
 class Car extends Vehicle {
-  constructor(name, type = "car", size = "15", ticketNumber, ticketPrice) {
-    super(name, type, size, ticketNumber, ticketPrice);
+  constructor(
+    name,
+    type = "car",
+    size = "15",
+    ticketNumber,
+    ticketPrice,
+    parkStatus,
+    parkingTime
+  ) {
+    super(name, type, size, ticketNumber, ticketPrice, parkStatus, parkingTime);
   }
 }
 
 class Motor extends Vehicle {
-  constructor(name, type = "motor", size = "2.5", ticketNumber, ticketPrice) {
-    super(name, type, size, ticketNumber, ticketPrice);
+  constructor(
+    name,
+    type = "motor",
+    size = "2.5",
+    ticketNumber,
+    ticketPrice,
+    parkStatus,
+    parkingTime
+  ) {
+    super(name, type, size, ticketNumber, ticketPrice, parkStatus, parkingTime);
   }
 }
 
 class TicketingMachine {
-  constructor(number = 1, price, ticketAvailable = 200) {
-    this.number = number;
-    this.price = price;
-    this.ticketAvailable = ticketAvailable;
+  constructor(name, number = 1, price = 2000, ticketAvailable = 200) {
+    this.name = name;
+    this.number = parseFloat(number);
+    this.price = parseFloat(price);
+    this.ticketAvailable = parseFloat(ticketAvailable);
   }
 
   printTicket(parkingArea, vehicle) {
     if (this.ticketAvailable > 0) {
-      if (parkingArea.sizeOfParkingArea >= vehicle.size) {
+      if (parkingArea.sizeOfParkingArea - vehicle.size >= 0) {
         parkingArea.sizeOfParkingArea -= vehicle.size;
         this.ticketAvailable--;
-        vehicle.ticketNumber = this.number;
+        vehicle.ticketNumber = `${this.name}-${this.number}`;
         this.number++;
         if (vehicle.type === "car") {
           vehicle.ticketPrice = 5000;
@@ -64,13 +103,46 @@ class TicketingMachine {
           return `${vehicle.name} enter ${parkingArea.name}. Ticket number is ${vehicle.ticketNumber} and ticket price is ${vehicle.ticketPrice}`;
         } else {
           vehicle.ticketPrice = 10000;
-          return `${vehicle.name} enter ${parkingArea.name}. Ticket number is ${vehicle.ticketNumber} and ticket price is ${vehicle.ticketPrice}`;
+          return `${vehicle.name} enter ${parkingArea.nameq33}. Ticket number is ${vehicle.ticketNumber} and ticket price is ${vehicle.ticketPrice}`;
         }
       } else {
-        return `Sorry, no more available space for ${ParkingArea.name} Parking Area. Please find another parking area.`;
+        return `Sorry, no more available space for ${parkingArea.name} Parking Area. Please find another parking area. ${vehicle}`;
       }
     } else {
       return `Sorry, we are closed. No more ticket for today.`;
     }
   }
+
+  pay(parkingArea, vehicle) {
+    let bill = vehicle.ticketPrice * vehicle.parkingTime;
+    parkingArea.cash += bill;
+    parkingArea.sizeOfParkingArea += parseFloat(vehicle.size);
+    vehicle.ticketPrice = 0;
+    vehicle.ticketNumber = null;
+    vehicle.parkingTime = 0;
+
+    return `Thank you. You pay : Rp${bill},00`;
+  }
 }
+
+// Test
+let Hartono = new ParkingArea("Hartono", "RR Utara", 2000);
+let Tm1 = new TicketingMachine("Tm1");
+let Tm2 = new TicketingMachine("Tm2");
+let Tm3 = new TicketingMachine("Tm3");
+let BMW = new Car("BMW");
+let Vario = new Motor("Vario");
+
+console.log(Hartono.greeting());
+console.log(BMW.parkIn(Hartono, Tm1));
+console.log(Vario.parkIn(Hartono, Tm2));
+console.log(BMW.setParkingTime(3));
+console.log(BMW.parkOut(Hartono, Tm2));
+console.log(Vario.setParkingTime(2));
+console.log(Vario.parkOut(Hartono, Tm1));
+console.log(BMW.parkIn(Hartono, Tm2));
+console.log(Vario.parkIn(Hartono, Tm2));
+
+console.log(BMW);
+console.log(Vario);
+console.log(Hartono);
